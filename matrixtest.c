b53fd9115361e78a633508d76fe1d1858e17783c
Adam <afranklint16@gmail.com>
2018-05-13 22:07:05 -0700

    Some starter unit test code

--- matrixtest.c
+++ matrixtest.c
@@ -10,5 +10,24 @@
 
 int main()
 {
-	printf("Hello, world!");
+	Matrix a = createMatrix(3, 3);
+	Matrix b = createMatrix(2, 2);
+
+	set(a, 1, 1, 1);
+	set(a, 1, 2, 2);
+	set(a, 1, 3, 3);
+	set(a, 2, 1, 4);
+	set(a, 2, 2, 5);
+	set(a, 2, 3, 6);
+	set(a, 3, 1, 7);
+	set(a, 3, 2, 8);
+	set(a, 3, 3, 9);
+
+	set(b, 1, 1, -1);
+	set(b, 1, 2, -2);
+	set(b, 2, 1, 0.0);
+	set(b, 2, 2, 100);
+
+	print(a);
+	print(b);
 }
